fb28a65328e5b8b10cbb380f8e8a6f2a5eb5e732
RohitGJ <77436246+rohitgj169@users.noreply.github.com>
2021-06-10 14:57:58 -0400

    added server

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+
+require('dotenv').config();
+require('./config/database');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+//API routes
+
+//Catch all route
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, function() {
+  console.log(`Express app running on port ${port}`)
+});
+
