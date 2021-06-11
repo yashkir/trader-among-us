e07de6a3a6ab987348173b9f8f53cfe255d2e44a
RohitGJ <77436246+rohitgj169@users.noreply.github.com>
2021-06-11 14:20:07 -0400

    token implemented

--- src/utils/users-service.js
+++ src/utils/users-service.js
@@ -3,6 +3,7 @@ import * as usersAPI from './users-api';
 export default async function signUp(userData) {
   try {
     const token = await usersAPI.signUp(userData)
+    localStorage.setItem('token', token);
     return token;
   } catch {
     throw new Error('Invalid Sign Up');
