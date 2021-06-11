11b95196f455197b3a1320e65f915ff7fcee3bdb
RohitGJ <77436246+rohitgj169@users.noreply.github.com>
2021-06-11 19:19:07 -0400

    login done

--- src/utils/users-api.js
+++ src/utils/users-api.js
@@ -1,7 +1,6 @@
 const BASE_URL = "/api/users";
 
 export async function signUp(userData) {
-  
   const res = await fetch(BASE_URL, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
@@ -11,5 +10,18 @@ export async function signUp(userData) {
     return res.json();
   } else {
     throw new Error("Invalid Sign Up");
+  }
+}
+
+export async function login(credentials) {
+  const res = await fetch(`${BASE_URL}/login`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(credentials),
+  });
+  if (res.ok) {
+    return res.json();
+  } else {
+    throw new Error('Bad Request');
   }
 }
