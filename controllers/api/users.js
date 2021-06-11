f259fbebec616e4517311224e42949277f1982c2
RohitGJ <77436246+rohitgj169@users.noreply.github.com>
2021-06-11 19:19:07 -0400

    login done

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,5 +1,6 @@
 const User = require("../../models/user");
 const jwt = require("jsonwebtoken");
+const bcrypt = require('bcrypt');
 
 const secret = process.env.SECRET;
 
@@ -13,10 +14,23 @@ const create = async (req, res) => {
   }
 };
 
+async function login(req, res) {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!(await bcrypt.compare(req.body.password, user.password))) throw new Error();
+    const token = createJWT(user);
+
+    res.status(200).json(token);
+  } catch {
+    res.status(400).json("Bad Credentials");
+  }
+}
+
 function createJWT(user) {
   return jwt.sign({ user }, secret, { expiresIn: "24h" });
 }
 
 module.exports = {
   create,
+  login,
 };
--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import SignUpForm from "../../components/SignUpForm/SignUpForm";
+import LoginForm from "../../components/LoginForm/LoginForm";
 
 export default function AuthPage({ setUser }) {
+  const [showLogin, setShowLogin] = useState(false);
+
   return (
     <div>
       <h1>AuthPage</h1>
-      <SignUpForm setUser={setUser} />
+      <button onClick={() => setShowLogin(!showLogin)}>{showLogin ? 'Sign Up': 'Log In'}</button>
+      {showLogin ? <LoginForm setUser={setUser} /> : <SignUpForm setUser={setUser} />}
     </div>
   );
 }
