148edb54c0823640952ce20c01ef0394d7f1fea4
Ben Shekhtman <benshekhtman@hotmail.com>
2021-06-11 20:28:06 -0400

    dropdown HOME btn routes to posts index, NEW ITEM btn routes to new post form

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,4 +1,5 @@
 import { FaUser, FaHandshake, FaPen, FaHome } from "react-icons/fa";
+import { Link } from 'react-router-dom'
 import "./NavBar.css";
 
 const NavBar = ({ handleMenu, handleSecondMenu }) => {
@@ -21,7 +22,9 @@ const NavBar = ({ handleMenu, handleSecondMenu }) => {
           <FaPen id="nav-icon" onClick={handleSecondMenu} />
         </div>
         <div className="icon-contain">
-          <FaHome id="nav-icon" />
+          <Link to="/posts">
+            <FaHome id="nav-icon" />
+          </Link>
         </div>
       </nav>
     </>
