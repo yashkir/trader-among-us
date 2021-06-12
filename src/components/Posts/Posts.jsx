1cdba1f575408f8d44168de3b6b4903bceed7cb0
Yuriy Yashkir <yuriy.yashkir@gmail.com>
2021-06-12 15:02:56 -0400

    load posts from posts-api to show on the /posts page

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -1,18 +1,60 @@
+import { useState, useEffect } from 'react';
 import './Posts.css'
 import PageTitle from "../../components/PageTitle/PageTitle"
+import postsApi from "../../utils/posts-api";
 
-const Posts = ({ title, description, image }) => {
+const Posts = (props) => {
+  // TODO API Calls and posts state are temporarily here, but should be moved up to a Page
+  const [posts, setPosts] = useState([]);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  // Load posts here with an API call on component mount.
+  // Using a promise since useEffect must be synchronous.
+  useEffect(() => {
+    postsApi.getAllPosts()
+      .then(data => setPosts(data))
+      .catch(err => setErrorMsg(err.message))
+  }, []);
+
   return (
     <>
       <PageTitle titleOne={"VIEW"} titleTwo={"POSTS"} />
 
+      {/* TODO style this or make a new Error component, simply shows any errors */}
+      {errorMsg ? (<p>{errorMsg}</p>) : null}
+
+      {/* Showing all posts */}
+      <div>
+        {posts.length ? posts.map(post => {
+          return (
+            <div key={post._id}>
+              <div className="row">
+                <div className="column-img">
+                  {/* TODO this is a placeholder image, remove */}
+                  <div><img id="post-img" src="https://i.stack.imgur.com/BOSno.jpg" alt="placeholder"></img></div>
+                </div>
+                <div className="column-text">
+                  <div className="post-title">{post.title}</div>
+                  <p className='flex-p'>{post.text}</p>
+                </div>
+                <div className="column-btn">
+                  <div className="post-btn">View More</div>
+                </div>
+              </div>
+              <hr />
+            </div>
+          )
+        }) : null }
+      </div>
+
+      {/* TODO these are sample posts, remove them */}
       {/*POST ONE */}
       <div className="row">
         <div className="column-img">
           <div><img id="post-img" src="https://i.stack.imgur.com/BOSno.jpg"></img></div>
         </div>
         <div className="column-text">
-          <div className="post-title">{title}</div>
+          <div className="post-title">OLD BIKE FROM MY GRANDMA</div>
           <p className='flex-p'>My grandmas bike is the best guys. THE BEST EVER!!</p>
         </div>
         <div className="column-btn">
