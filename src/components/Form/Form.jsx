4b99e94b5d7e208e813d3e30873a2f41b54777f0
Yuriy Yashkir <yuriy.yashkir@gmail.com>
2021-06-12 16:44:46 -0400

    implement Form -> posts-api.create() call
    We can create new posts now, with no client-side verification.

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,24 +1,73 @@
+import { useState } from 'react';
 import "./Form.css"
 import { FaFileUpload } from "react-icons/fa";
 import PageTitle from "../../components/PageTitle/PageTitle"
+import postsApi from "../../utils/posts-api";
 
 
 const Form = ({ name, description, image }) => {
+  const [inputValues, setInputValues] = useState({
+    title: "",
+    text: "",
+    image: "",
+  });
+
+  const [status, setStatus] = useState(null);
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setInputValues({...inputValues, [e.target.name]: e.target.value});
+  };
+
+  /* FIXME we are not doing any client-side verification here, just calling the API*/
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await postsApi.create({
+        title: inputValues.title,
+        text: inputValues.text,
+      });
+      setStatus(response);
+    } catch (err) {
+      setStatus(err.message);
+    }
+  }
+
   return (
     <div className="form-container">
 
       <PageTitle titleOne={"NEW"} titleTwo={"POST"} />
 
+      {/* TODO style this error message */}
+      {status ? <p>{status}</p> : null}
+
       <section id="form-sec">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="form-border">
-            <label for="" className="form-label">Name</label>
-            <input type="text" className="form-input" />
-            <label for="" className="form-label">Description</label>
-            <textarea rows="6" name="des" />
-            <label for="" className="form-label">Image</label>
-            <label class="custom-file-upload">
-              <input type="file" />
+            <label for="title" className="form-label">Title</label>
+            <input id="title"
+              name="title"
+              type="text"
+              className="form-input"
+              value={inputValues.title}
+              onChange={handleChange}
+            />
+            <label for="description" className="form-label">Description</label>
+            <textarea
+              id="text"
+              rows="6"
+              name="text"
+              value={inputValues.text}
+              onChange={handleChange}
+            />
+            <label for="image" className="form-label">Image</label>
+            <label id="image" class="custom-file-upload">
+              <input
+                type="file"
+                name="image"
+                value={inputValues.image}
+                onChange={handleChange}
+              />
               <FaFileUpload id="upload" />
             </label><br />
           </div>
@@ -26,9 +75,8 @@ const Form = ({ name, description, image }) => {
       </section>
 
       <div className="btn-div">
-        <input className="submit-btn" type="submit" value="SUBMIT"></input>
+        <input className="submit-btn" type="submit" value="SUBMIT" onClick={handleSubmit}></input>
       </div>
-
     </div>
   )
 }
--- src/utils/posts-api.js
+++ src/utils/posts-api.js
@@ -11,6 +11,26 @@ async function getAllPosts() {
   }
 }
 
+async function create(data) {
+  const payload = {
+    title: data.title,
+    text: data.text,
+  }
+
+  const res = await fetch(BASE_URL, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(payload),
+  });
+
+  if (res.ok) {
+    return "Post created successfully."
+  } else {
+    throw new Error("Unable to create post.");
+  }
+}
+
 module.exports = {
-  getAllPosts
+  getAllPosts,
+  create,
 }
