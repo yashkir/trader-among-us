013f382ef173b1700149b2c3187fbb07b0f2a901
Ben Shekhtman <benshekhtman@hotmail.com>
2021-06-12 11:18:52 -0400

    styled and connecting the page

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -2,16 +2,21 @@ import React, { useState } from "react";
 import SignUpForm from "../../components/SignUpForm/SignUpForm";
 import PageTitle from "../../components/PageTitle/PageTitle"
 import LoginForm from "../../components/LoginForm/LoginForm";
+import './AuthPage.css'
 
 export default function AuthPage({ setUser }) {
   const [showLogin, setShowLogin] = useState(false);
 
   return (
     <div>
-      <PageTitle titleOne={"SIGN"} titleTwo={"UP"} />
-      <h1>AuthPage</h1>
-      <button onClick={() => setShowLogin(!showLogin)}>{showLogin ? 'Sign Up': 'Log In'}</button>
+      {!showLogin ? <PageTitle titleOne={"SIGN"} titleTwo={"UP"} /> : <PageTitle titleOne={"LOG"} titleTwo={"IN"} />}
       {showLogin ? <LoginForm setUser={setUser} /> : <SignUpForm setUser={setUser} />}
+      {!showLogin ? <p className="AuthPage log-p">Already Have an Account?</p> : <p className="AuthPage log-p">Don't Have an Account?</p>}
+      <div className='AuthPage btn-contain-flex'>
+        <div className='AuthPage ctr-btn'>
+          <button className="AuthPage log-toggle" onClick={() => setShowLogin(!showLogin)}>{showLogin ? 'Sign Up' : 'Log In'}</button>
+        </div>
+      </div>
     </div>
   );
 }
