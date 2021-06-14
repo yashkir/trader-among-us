cb401d4a415e285a00563369544e3a0cd422eaae
RohitGJ <77436246+rohitgj169@users.noreply.github.com>
2021-06-14 19:29:14 -0400

    changes

---
+++ src/pages/UesrProfilePage/UesrProfilePage.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './UserProfilePage.css';
+
+export default function UserProfilePage() {
+  return (
+    <>
+      <div className="user-profile-container">
+        <div className="user-profile-heading">
+          <h1>UserProfilePage</h1>
+        </div>
+        <div className="user-profile-nav">
+          <ul>
+            <li>View Profile</li>
+            <li>View Items</li>
+            <li>Add Items</li>
+          </ul>
+        </div>
+        <div className="user-profile-body">
+          COMPONENT
+        </div>
+      </div>
+      
+    </>
+  )
+}
