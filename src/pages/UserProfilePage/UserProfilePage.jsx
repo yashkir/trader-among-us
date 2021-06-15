269674f36e4beac447c3ad03746258d195be67f9
RohitGJ <77436246+rohitgj169@users.noreply.github.com>
2021-06-15 13:34:54 -0400

    item carousel

--- src/pages/UserProfilePage/UserProfilePage.jsx
+++ src/pages/UserProfilePage/UserProfilePage.jsx
@@ -1,27 +1,36 @@
 import React from 'react';
 import ItemCreateForm from '../../components/ItemCreateForm/ItemCreateForm';
 import PageTitle from "../../components/PageTitle/PageTitle"
-import { Route, Link } from 'react-router-dom';
+import { Route, Link, Switch } from 'react-router-dom';
 import './UserProfilePage.css';
+import Carousel from '../../components/Carousel/Carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 export default function UserProfilePage() {
-  
   return (
     <>
-    <PageTitle titleOne={"USER"} titleTwo={"PROFILE"} />
+      <PageTitle titleOne={"USER"} titleTwo={"PROFILE"} />  
       <div className="user-profile-container">
-        <div className="user-profile-heading">
-          <h1>Test</h1>
-        </div>
         <div className="user-profile-nav">
-          <ul>
-            <li><Link to="/users/profile">View Profile</Link></li>
-            <li><Link to="/users/items" exact >View Items</Link></li>
-            <li><Link to="/users/items/new">Add Items</Link></li>
-          </ul>
+          <div>
+            <Link to="/users/profile">View Profile</Link>
+          </div>
+          <div>
+            <Link to="/users/items" exact >View Items</Link>
+          </div>
+          <div>
+            <Link to="/users/items/new">Add Items</Link>
+          </div>
         </div>
+        <div className="user-profile-heading">
+          {/* <h1>Test</h1> */}
+        </div>
+
         <div className="user-profile-body">
-          <Route path="/users/items/new" component={ItemCreateForm} />
+          <Switch>
+            <Route path="/users/items/new" component={ItemCreateForm} />
+            <Route path="/users/items" component={Carousel} />
+          </Switch>
         </div>
       </div>
       
